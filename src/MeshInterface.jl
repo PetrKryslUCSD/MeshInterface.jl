a0659f04c26cc5a1fd2f7108db1a83dc8f5a21d3
Petr Krysl <pkrysl@ucsd.edu>
2020-01-27 20:16:08 -0800

    A bit of documentation

--- src/MeshInterface.jl
+++ src/MeshInterface.jl
@@ -7,12 +7,35 @@ fesettrait(a) = NoSuchTrait()
 abstract type FEMeshTrait end
 femeshtrait(a) = NoSuchTrait()
 
+"""
+    numberofsets(x::T) where T
+
+Number of finite element sets in the mesh.  
+"""
 numberofsets(x::T) where T = _numberofsets(femeshtrait(T), x)
 _numberofsets(::NoSuchTrait, x) = 0
+
+"""
+    numberofelements(x::T) where T
+
+Number of elements in a finite element set.  
+"""
 numberofelements(x::T) where T = _numberofelements(fesettrait(T), x)
 _numberofelements(::NoSuchTrait, x) = 0
+
+"""
+    numberofnodesperelement(x::T) where T
+
+Number of nodes per element.  
+"""
 numberofnodesperelement(x::T) where T = _numberofnodesperelement(fesettrait(T), x)
 _numberofnodesperelement(::NoSuchTrait, x) = 0
+
+"""
+    getconn!(c::C, fes::T, i) where {C, T}
+
+Retrieve connectivity of the `i`th element in the set.  
+"""
 getconn!(c::C, fes::T, i) where {C, T} = _getconn!(fesettrait(T), c::C, fes::T, i)
 _getconn!(::NoSuchTrait, c::C, fes::T, i) where {C, T} = c
 
