584c5913feb08553dd74f7a9491289da1f9577ed
Petr Krysl <pkrysl@ucsd.edu>
2020-01-27 17:15:03 -0800

    More functionality

--- src/MeshInterface.jl
+++ src/MeshInterface.jl
@@ -7,9 +7,16 @@ fesettrait(a) = NoSuchTrait()
 struct FEMeshTrait end
 femeshtrait(a) = NoSuchTrait()
 
+numberofsets(x::T) where T = _numberofsets(femeshtrait(T), x)
+_numberofsets(::NoSuchTrait, x) = 0
 numberofelements(x::T) where T = _numberofelements(fesettrait(T), x)
 _numberofelements(::NoSuchTrait, x) = 0
+numberofnodesperelement(x::T) where T = _numberofnodesperelement(fesettrait(T), x)
+_numberofnodesperelement(::NoSuchTrait, x) = 0
 getconn!(c::C, fes::T, i) where {C, T} = _getconn!(fesettrait(T), c::C, fes::T, i)
 _getconn!(::NoSuchTrait, c::C, fes::T, i) where {C, T} = c
+
+
+
 
 end # module
